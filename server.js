adb83f91330af47119c6dbdfec54d1bf0e708275
Saurav Gupta <gsaurav641@gmail.com>
2026-06-02 11:03:51 +0530

    WIP

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
+});
